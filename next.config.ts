21e9f197cca512f1de64bbc34b4ede6a093542c8
garvitchoudharyy <garvitc58@gmail.com>
2026-06-09 18:52:51 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // This allows production builds to successfully complete 
-    // even if your project has TypeScript errors.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // This blocks ESLint errors from stopping your build as well
     ignoreDuringBuilds: true,
   },
 };
